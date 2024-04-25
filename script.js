c2c6db1b9c421885c648b93aa57d87e0164268a1
AnaLuiza <aninha141108@gmail.com>
2024-04-24 23:52:10 -0300

    implementa possível código funcional em JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function validateForm(event) {
     event.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    // Alterei os const para var, para verificar se vai mudar algo 
 
     // Validação simples: Verifica se os campos não estão vazios
     if (name === '' || email === '' || password === '') {
@@ -15,7 +16,19 @@ function validateForm(event) {
     if (!emailPattern.test(email)) {
         alert('Por favor, insira um endereço de e-mail válido.');
         return;
-    }
-
-    // Se chegou até aqui, o formulário e
+    }else
     alert('Cadastro bem-sucedido!');}
+    
+    function logar(){
+
+        var name = document.getElementById('name').value;
+        var password = document.getElementById('password').value;
+        var email = document.getElementById('email').value;
+    
+        if(name == "" || email == "" || password == ""){
+            alert("Por favor, preencha todos os campos")
+        }else{
+            alert("Bem-vindo(a)!")
+        }
+    
+    }
