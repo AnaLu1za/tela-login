d9632932ebf936195ef1d47de87fe475d9222a05
AnaLu1za <aninha141108@gmail.com>
2024-04-25 10:52:51 -0300

    finaliza código em JavaScript

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
 function validateForm(event) {
     event.preventDefault();
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    // Alterei os const para var, para verificar se vai mudar algo 
-
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    
     // Validação simples: Verifica se os campos não estão vazios
-    if (name === '' || email === '' || password === '') {
+    if (name == '' || email == '' || password == '') {
         alert('Por favor, preencha todos os campos.');
         return;
     }
-
-    // Validação do formato de e-mail usando uma expressão regular simples
+   
     const emailPattern = /^\S+@\S+\.\S+$/;
     if (!emailPattern.test(email)) {
         alert('Por favor, insira um endereço de e-mail válido.');
         return;
-    }else
-    alert('Cadastro bem-sucedido!');}
-    
-    function logar(){
-
-        var name = document.getElementById('name').value;
-        var password = document.getElementById('password').value;
-        var email = document.getElementById('email').value;
-    
-        if(name == "" || email == "" || password == ""){
-            alert("Por favor, preencha todos os campos")
-        }else{
-            alert("Bem-vindo(a)!")
-        }
-    
-    }
+    }else{
+    alert('Cadastro bem-sucedido!');
+    window.location.href='index.html'
+}
+}  
